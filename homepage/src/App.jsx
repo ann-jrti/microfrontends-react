4b5e590cbc15e24f8622b70ec3403fda9c6ce8d2
Noah Young <noah.young@sqa-consulting.com>
2022-08-22 15:06:45 +0200

    added routes

--- homepage/src/App.jsx
+++ homepage/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Content } from './Content';
 import { Footer } from './Footer';
 import { Header } from './Header';
@@ -8,9 +9,24 @@ import './index.css';
 
 const App = () => (
   <div className="container">
-    <Header />
-    <Content contentMessage="test" />
-    <Footer />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route
+          path='/'
+          element={<Content contentMessage="test" />} />
+
+        <Route
+          path='/react'
+          element={<Content contentMessage="React" />} />
+        
+        <Route
+          path='/vue'
+          element={<Content contentMessage="Vue" />} />
+      </Routes>
+
+      <Footer />
+    </BrowserRouter>
   </div>
 );
 ReactDOM.render(<App />, document.getElementById('app'));
